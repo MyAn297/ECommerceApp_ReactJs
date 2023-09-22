c3aa27db69a52109af575cf9b5561b231467fb9f
An <an@rnds-iMac.local>
2023-09-22 09:22:09 +0700

    filter-card

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
 
 function OurStore() {
   return (
@@ -53,34 +54,155 @@ function OurStore() {
                   </div>
                   <h5 className="sub-title">Price</h5>
                   <div className="d-flex align-items-center gap-10">
-                    <div class="form-floating mb-3 d-flex align-items-center">
-                      $
-                      <input
-                        type="number"
-                        class="form-control"
-                        id="floatingInput"
-                        placeholder="From"
-                      />
-                      <label for="floatingInput">From</label>
+                    <div class="form-floating mb-3 d-flex align-items-center gap-10">
+                      <span className="">$</span>
+                      <div>
+                        <input
+                          type="number"
+                          class="form-control"
+                          id="floatingInput"
+                          placeholder="From"
+                        />
+                      </div>
                     </div>
-                    <div class="form-floating mb-3 d-flex align-items-center">
-                      $
+                    <div class="form-floating mb-3 d-flex align-items-center gap-10">
+                      <span className="">$</span>
+                      <div>
+                        <input
+                          type="number"
+                          class="form-control"
+                          id="floatingInput1"
+                          placeholder="To"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Colors</h5>
+                  <div className="">
+                    <ul className="colors ps-0">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </div>
+                  <h5 className="sub-title">Size</h5>
+                  <div>
+                    <div className="form-check">
                       <input
-                        type="number"
-                        class="form-control"
-                        id="floatingInput1"
-                        placeholder="To"
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
                       />
-                      <label for="floatingInput1">To</label>
+                      <label className="form-check-lable" htmlFor="">
+                        S (10)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                        checked
+                      />
+                      <label className="form-check-lable" htmlFor="">
+                        M (13)
+                      </label>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Product Tag</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Headphones
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Oppo
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Speaker
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Tablet
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Vivo
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Wire
+                    </span>
+                  </div>
+                </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Products</h3>
+                <div>
+                  <div className="randon-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <p className="price">$100.00</p>
+                    </div>
+                  </div>
+                  <div className="randon-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <p className="price">$100.00</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="col-9"></div>
