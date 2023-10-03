bd3a50f61e4299f290edaed9a5b0e2ab86512ec9
An <an@rnds-iMac.local>
2023-10-03 16:36:01 +0700

    account page

--- src/pages/Resetpassword.js
+++ src/pages/Resetpassword.js
@@ -3,27 +3,19 @@ import { Link } from "react-router-dom";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 
-function Login() {
+function Resetpassword() {
   return (
     <>
-      <Meta title={"Login"} />
-      <BreadCrumb title="Login" />
+      <Meta title={"Reset Password"} />
+      <BreadCrumb title="Reset Password" />
       <div className="login-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
               <div className="auth-card">
-                <h3 className="text-center">Login</h3>
+                <h3 className="text-center">Reset Password</h3>
                 <form action="" className="d-flex flex-column gap-15">
                   <div>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      className="form-control"
-                    />
-                  </div>
-                  <div className="mt-1">
                     <input
                       type="password"
                       name="password"
@@ -31,13 +23,17 @@ function Login() {
                       className="form-control"
                     />
                   </div>
+                  <div className="mt-1">
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Confirm Password"
+                      className="form-control"
+                    />
+                  </div>
                   <div>
-                    <Link to="/forgot-password">Forgot Password?</Link>
                     <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                      <button className="button border-0">Login</button>
-                      <Link to="/signup" className="button signup">
-                        SignUp
-                      </Link>
+                      <button className="button border-0">Ok</button>
                     </div>
                   </div>
                 </form>
@@ -50,4 +46,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Resetpassword;
